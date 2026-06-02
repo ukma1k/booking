c5d979672dc9127ec53bd9aba6b35efdc106941d
ukma1k <boculakmarta183@gmail.com>
2026-06-02 20:35:01 +0300

    .

--- booking_app/urls.py
+++ booking_app/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from booking_app import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('booking/<int:apartment_id>/', views.booking_page, name='booking_page'),
 ]
--- booking_app/views.py
+++ booking_app/views.py
@@ -3,3 +3,11 @@ from booking_app.models import Category, Apartments, Booking
 def home(request):
     apartments = Apartments.objects.all()
     return render(request, 'index.html', {"name": "Booking App", "apartments": apartments})
+
+
+def booking_page(request, apartment_id):
+
+    apartment = Apartments.objects.get(id=apartment_id)
+    context = {"apartment": apartment}
+    
+    return render(request, 'booking.html', context)

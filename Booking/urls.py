ed19732fad1faec0cbcc8862dc9218e9c88e72a5
ukma1k <boculakmarta183@gmail.com>
2026-06-16 20:31:23 +0300

    //

--- Booking/urls.py
+++ Booking/urls.py
@@ -21,4 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('booking_app.urls')),
+    path('', include('auth_system.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
